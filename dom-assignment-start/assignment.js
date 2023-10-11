5e3cf0d00c04df45ea67577304cbe3f065805e7a
Hasan Altaf <147080959+HasanAltaf@users.noreply.github.com>
2023-10-11 13:31:35 +0500

    Add files via upload

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,7 @@
+const li1 = document.getElementById('task-1');
+li1.style.color='white';
+li1.style.backgroundColor='black';
+const h11= document.querySelector('title');
+h11.textContent='Assignment-Solved!';
+const h111= document.querySelector('h1');
+h111.textContent='Assignment-Solved!';
